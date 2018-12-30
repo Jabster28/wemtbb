ccab508097bb69cb85a4c0e67b504a728e1637db
Jabster28 <justynboyer@gmail.com>
2018-12-30 21:32:23 +0000

    added straming

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.tkn;
 const fs = require('fs');
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
 const ffmpeg = require("ffmpeg")
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./scores.sqlite');
@@ -237,230 +239,235 @@ client.on('message', msg => {
 
 // -join
 client.on('message', msg => {
-  if (isOk(msg)) {
-    if (msg.content.toLowerCase() == "-join") {
-      if (msg.member.voiceChannel) {
-        msg.member.voiceChannel.join()
-        msg.channel.send("Successfully joined the channel")
-      } else if (!(msg.member.voiceChannel.joinable)) {
-        msg.channel.send("Hmm, I can't access this channel. Please tell the server owner about this")
-      } else
-        msg.channel.send("Sorry, but you must be in a Voice Channel to use that! Why not join one?")
-    }
-  }
-});
+      if (isOk(msg)) {
+        mess = msg.content.splice(" ")
+        if (mess[0] == "-join") {
+          if (msg.member.voiceChannel) {
+            msg.member.voiceChannel.join().then(connection => {
+                const stream = ytdl(mess[1], {
+                  filter: 'audioonly'
+                });
+                const dispatcher = connection.playStream(stream, streamOptions);
+                msg.channel.send("Successfully joined the channel")
+              } else if (!(msg.member.voiceChannel.joinable)) {
+                msg.channel.send("Hmm, I can't access this channel. Please tell the server owner about this")
+              } else
+                msg.channel.send("Sorry, but you must be in a Voice Channel to use that! Why not join one?")
+            }
+          }
+        });
 
-// -leave
-client.on('message', msg => {
-  if (isOk(msg)) {
-    if (msg.content.toLowerCase() == "-leave") {
-      if (guild.me.voiceChannel) {
-          guild.me.voiceChannel.leave()
-          msg.channel.send("Goodbye!")
-      }
-    }
-  }
-});
+      // -leave
+      client.on('message', msg => {
+        if (isOk(msg)) {
+          if (msg.content.toLowerCase() == "-leave") {
+            if (guild.me.voiceChannel) {
+              guild.me.voiceChannel.leave()
+              msg.channel.send("Goodbye!")
+            }
+          }
+        }
+      });
 
-// -deletechannel
-client.on('message', msg => {
-  if (isOk(msg)) {
-    mess = msg.content.toLowerCase().split(" ");
-    if (mess[0] == "-deletechannel") {
-      if (hasModPerms(msg)) {
-        channame = msg.mentions.channels.array()[0].name
-        role = roleFind(msg.guild.roles.array(), arrayObjFindByName(stuff.channels, msg.mentions.channels.array()[0].name).role)
-        rolename = role.name;
-        role.delete()
-        msg.mentions.channels.array()[0].delete().then(channel => {
-          msg.delete()
-          embed = new Discord.RichEmbed();
-          embed.setTitle("Successfully deleted channel and associated role")
-          embed.addField("Deleted Channel:", channame)
-          embed.addField("Deleted Role:", rolename)
-          embed.setAuthor(msg.author.username, msg.author.authorURL)
-          embed.setColor("BLUE")
-          embed.setFooter("Made by Jabster28, made for Ramoth")
-          msg.channel.send(embed).then(msg => msg.delete(5000))
+      // -deletechannel
+      client.on('message', msg => {
+        if (isOk(msg)) {
+          mess = msg.content.toLowerCase().split(" ");
+          if (mess[0] == "-deletechannel") {
+            if (hasModPerms(msg)) {
+              channame = msg.mentions.channels.array()[0].name
+              role = roleFind(msg.guild.roles.array(), arrayObjFindByName(stuff.channels, msg.mentions.channels.array()[0].name).role)
+              rolename = role.name;
+              role.delete()
+              msg.mentions.channels.array()[0].delete().then(channel => {
+                msg.delete()
+                embed = new Discord.RichEmbed();
+                embed.setTitle("Successfully deleted channel and associated role")
+                embed.addField("Deleted Channel:", channame)
+                embed.addField("Deleted Role:", rolename)
+                embed.setAuthor(msg.author.username, msg.author.authorURL)
+                embed.setColor("BLUE")
+                embed.setFooter("Made by Jabster28, made for Ramoth")
+                msg.channel.send(embed).then(msg => msg.delete(5000))
+              })
+
+            }
+          }
+        }
+      });
+
+      // -roll
+      client.on('message', msg => {
+        if (isOk(msg)) {
+          mess = msg.content.toLowerCase().split(" ");
+          if (mess[0] == "-roll") {
+            if (mess[1]) {
+              embed = new Discord.RichEmbed();
+              embed.setAuthor("Is rolling a Dice...", msg.author.avatarURL)
+              embed.setColor("BLUE")
+              embed.addField(("D" + mess[1] + ":"), (generateRandomNumber(mess[1]) + "!"));
+              msg.channel.send(embed)
+            } else {
+              embed = new Discord.RichEmbed();
+              embed.setAuthor("Is rolling a Dice...", msg.author.avatarURL)
+              embed.setColor("BLUE")
+              embed.addField("D6:", (generateRandomNumber(6) + "!"));
+              msg.channel.send(embed)
+            }
+          }
+        }
+      });
+
+      // -info || -help
+      client.on('message', msg => {
+        if (isOk(msg)) {
+          if (msg.content.toLowerCase() == "-info" || msg.content.toLowerCase() == "-help") {
+            embed = new Discord.RichEmbed();
+            embed.setTitle("RamBot, made for WEMT")
+            embed.setDescription("Hi, my name is RamBot and i'm a bot made for the **WEMT** tournament server! RamBot is created by Jabster28 and ramoth, and its coded by Jabster28.")
+            embed.setThumbnail("https://cdn.discordapp.com/attachments/507886876758245376/528996120966201348/rambot.png")
+            embed.setColor("GREEN")
+            embed.setFooter("Ask Ramoth#3304 for more info!\nDo -commands for commands")
+            msg.channel.send(embed)
+          }
+        }
+      });
+
+      // Login
+
+      client.login(token);
+
+
+      // PM2 Metrics
+
+      io.metric({
+        type: 'metric',
+        name: 'Accessible Servers',
+        value: function() {
+          return client.guilds.array().length;
+        }
+      });
+      io.metric({
+        type: 'metric',
+        name: 'Status',
+        value: function() {
+          return "N/A";
+          //  return client.user.presence.status;
+        }
+      });
+      io.metric({
+        type: 'metric',
+        name: 'Ping',
+        value: function() {
+          return client.ping;
+        }
+      });
+      io.metric({
+        type: 'metric',
+        name: 'Accessible Channels',
+        value: function() {
+          return client.channels.array().length;
+        }
+      });
+      io.metric({
+        type: 'metric',
+        name: 'Cached users',
+        value: function() {
+          return client.users.array().length;
+        }
+      });
+      // PM2 Actions
+      io.action('Logging Test', (cb) => {
+        console.log("test pm2 log")
+        cb("Test success");
+      });
+
+      io.action('Set Offline', (cb) => {
+        client.user.setPresence({
+          game: {
+            name: 'with discord.js'
+          },
+          status: 'invisible'
         })
+        cb("WEMTBB is now invisible\n");
 
-      }
-    }
-  }
-});
+      });
+      io.action('Set AFK', (cb) => {
+        client.user.setPresence({
+          game: {
+            name: 'with discord.js'
+          },
+          status: 'idle'
+        })
+        cb("WEMTBB is now Idle\n");
 
-// -roll
-client.on('message', msg => {
-  if (isOk(msg)) {
-    mess = msg.content.toLowerCase().split(" ");
-    if (mess[0] == "-roll") {
-      if (mess[1]) {
-        embed = new Discord.RichEmbed();
-        embed.setAuthor("Is rolling a Dice...", msg.author.avatarURL)
-        embed.setColor("BLUE")
-        embed.addField(("D" + mess[1] + ":"), (generateRandomNumber(mess[1]) + "!"));
-        msg.channel.send(embed)
-      } else {
-        embed = new Discord.RichEmbed();
-        embed.setAuthor("Is rolling a Dice...", msg.author.avatarURL)
-        embed.setColor("BLUE")
-        embed.addField("D6:", (generateRandomNumber(6) + "!"));
-        msg.channel.send(embed)
-      }
-    }
-  }
-});
+      });
+      io.action('Set Online', (cb) => {
 
-// -info || -help
-client.on('message', msg => {
-  if (isOk(msg)) {
-    if (msg.content.toLowerCase() == "-info" || msg.content.toLowerCase() == "-help") {
-      embed = new Discord.RichEmbed();
-      embed.setTitle("RamBot, made for WEMT")
-      embed.setDescription("Hi, my name is RamBot and i'm a bot made for the **WEMT** tournament server! RamBot is created by Jabster28 and ramoth, and its coded by Jabster28.")
-      embed.setThumbnail("https://cdn.discordapp.com/attachments/507886876758245376/528996120966201348/rambot.png")
-      embed.setColor("GREEN")
-      embed.setFooter("Ask Ramoth#3304 for more info!\nDo -commands for commands")
-      msg.channel.send(embed)
-    }
-  }
-});
+        ran = generateRandomNumber(5)
+        if (ran == 1) {
+          client.user.setPresence({
+            game: {
+              name: 'with discord.js',
+              party: {
+                size: [2, 2]
+              }
+            },
+            status: 'online'
+          })
+          cb("WEMTBB is now online\n");
 
-// Login
+        } else if (ran == 2) {
+          client.user.setPresence({
+            game: {
+              name: 'the kazoo (dooo dooo do do doo)',
+              party: {
+                size: [2, 2]
+              }
+            },
+            status: 'online'
+          })
+          cb("WEMTBB is now online\n");
 
-client.login(token);
+        } else if (ran == 3) {
+          client.user.setPresence({
+            game: {
+              name: 'Deal or no deal',
+              party: {
+                size: [2, 2]
+              }
+            },
+            status: 'online'
+          })
+          cb("WEMTBB is now online\n");
 
+        } else if (ran == 4) {
+          client.user.setPresence({
+            game: {
+              name: "a game you can't join",
+              party: {
+                size: [2, 2]
+              }
+            },
+            status: 'online'
+          })
+          cb("WEMTBB is now online\n");
 
-// PM2 Metrics
+        } else if (ran == 5) {
+          client.user.setPresence({
+            game: {
+              name: 'Chess with Zeus',
+              party: {
+                size: [2, 2]
+              }
+            },
+            status: 'online'
+          })
+          cb("WEMTBB is now online\n");
 
-io.metric({
-  type: 'metric',
-  name: 'Accessible Servers',
-  value: function() {
-    return client.guilds.array().length;
-  }
-});
-io.metric({
-  type: 'metric',
-  name: 'Status',
-  value: function() {
-    return "N/A";
-    //  return client.user.presence.status;
-  }
-});
-io.metric({
-  type: 'metric',
-  name: 'Ping',
-  value: function() {
-    return client.ping;
-  }
-});
-io.metric({
-  type: 'metric',
-  name: 'Accessible Channels',
-  value: function() {
-    return client.channels.array().length;
-  }
-});
-io.metric({
-  type: 'metric',
-  name: 'Cached users',
-  value: function() {
-    return client.users.array().length;
-  }
-});
-// PM2 Actions
-io.action('Logging Test', (cb) => {
-  console.log("test pm2 log")
-  cb("Test success");
-});
+        } else {
+          io.notify("Error: No matching number. Number was " + ran)
+          cb("ERROR, CHECK ISSUES LOG\n");
 
-io.action('Set Offline', (cb) => {
-  client.user.setPresence({
-    game: {
-      name: 'with discord.js'
-    },
-    status: 'invisible'
-  })
-  cb("WEMTBB is now invisible\n");
-
-});
-io.action('Set AFK', (cb) => {
-  client.user.setPresence({
-    game: {
-      name: 'with discord.js'
-    },
-    status: 'idle'
-  })
-  cb("WEMTBB is now Idle\n");
-
-});
-io.action('Set Online', (cb) => {
-
-  ran = generateRandomNumber(5)
-  if (ran == 1) {
-    client.user.setPresence({
-      game: {
-        name: 'with discord.js',
-        party: {
-          size: [2, 2]
         }
-      },
-      status: 'online'
-    })
-    cb("WEMTBB is now online\n");
-
-  } else if (ran == 2) {
-    client.user.setPresence({
-      game: {
-        name: 'the kazoo (dooo dooo do do doo)',
-        party: {
-          size: [2, 2]
-        }
-      },
-      status: 'online'
-    })
-    cb("WEMTBB is now online\n");
-
-  } else if (ran == 3) {
-    client.user.setPresence({
-      game: {
-        name: 'Deal or no deal',
-        party: {
-          size: [2, 2]
-        }
-      },
-      status: 'online'
-    })
-    cb("WEMTBB is now online\n");
-
-  } else if (ran == 4) {
-    client.user.setPresence({
-      game: {
-        name: "a game you can't join",
-        party: {
-          size: [2, 2]
-        }
-      },
-      status: 'online'
-    })
-    cb("WEMTBB is now online\n");
-
-  } else if (ran == 5) {
-    client.user.setPresence({
-      game: {
-        name: 'Chess with Zeus',
-        party: {
-          size: [2, 2]
-        }
-      },
-      status: 'online'
-    })
-    cb("WEMTBB is now online\n");
-
-  } else {
-    io.notify("Error: No matching number. Number was " + ran)
-    cb("ERROR, CHECK ISSUES LOG\n");
-
-  }
-});
+      });
